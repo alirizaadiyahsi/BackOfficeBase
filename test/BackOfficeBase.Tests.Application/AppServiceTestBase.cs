bb3450bf73cea35957d6bd05c6bd2042a924eca4
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2020-04-21 01:44:28 +0300

    Minor refactoring on application service permissions test

--- test/BackOfficeBase.Tests.Application/AppServiceTestBase.cs
+++ test/BackOfficeBase.Tests.Application/AppServiceTestBase.cs
@@ -1,59 +1,11 @@
-using System.Collections.Generic;
-using System.Security.Claims;
-using System.Threading;
-using BackOfficeBase.Domain.AppConsts.Authorization;
 using BackOfficeBase.Domain.Entities.Authorization;
 using BackOfficeBase.Tests.Shared;
-using Microsoft.AspNetCore.Identity;
-using Moq;
 
 namespace BackOfficeBase.Tests.Application
 {
     public class AppServiceTestBase : TestBase
     {
-        protected readonly UserManager<User> UserManager;
-        protected readonly RoleManager<Role> RoleManager;
-
-        public AppServiceTestBase()
-        {
-            var testUser = GetTestUser();
-            var testRole = GetTestRole();
-            AddUserToRole(testUser, testRole);
-
-            var mockUserClaimStore = SetupMockUserClaimStore(testUser);
-            var mockRoleClaimStore = SetupMockRoleClaimStore(testRole);
-
-            var userManager = new UserManager<User>(mockUserClaimStore.Object, null, null, null, null, null, null, null, null);
-            var roleManager = new RoleManager<Role>(mockRoleClaimStore.Object, null, null, null, null);
-
-            UserManager = userManager;
-            RoleManager = roleManager;
-        }
-
-        private static Mock<IRoleClaimStore<Role>> SetupMockRoleClaimStore(Role testRole)
-        {
-            var mockRoleClaimStore = new Mock<IRoleClaimStore<Role>>();
-            mockRoleClaimStore.Setup(x => x.GetClaimsAsync(testRole, CancellationToken.None)).ReturnsAsync(
-                new List<Claim>
-                {
-                    new Claim(CustomClaimTypes.Permission, "TestPermissionForRoleClaim")
-                });
-            return mockRoleClaimStore;
-        }
-
-        private static Mock<IUserClaimStore<User>> SetupMockUserClaimStore(User testUser)
-        {
-            var mockUserClaimStore = new Mock<IUserClaimStore<User>>();
-            mockUserClaimStore.Setup(x => x.FindByNameAsync(testUser.UserName, CancellationToken.None)).ReturnsAsync(testUser);
-            mockUserClaimStore.Setup(x => x.GetClaimsAsync(testUser, CancellationToken.None)).ReturnsAsync(
-                new List<Claim>
-                {
-                    new Claim(CustomClaimTypes.Permission, "TestPermissionForUserClaim")
-                });
-            return mockUserClaimStore;
-        }
-
-        private static void AddUserToRole(User testUser, Role testRole)
+        public static void AddUserToRole(User testUser, Role testRole)
         {
             var testUserRole = new UserRole
             {
@@ -65,17 +17,17 @@ namespace BackOfficeBase.Tests.Application
             testRole.UserRoles.Add(testUserRole);
         }
 
-        private static Role GetTestRole(string roleName = "TestRoleName")
+        public static Role GetTestRole(string roleName = "TestRoleName")
         {
             var testRole = new Role
             {
-                Name = "TestRoleName"
+                Name = roleName
             };
 
             return testRole;
         }
 
-        private static User GetTestUser(string userName = "TestUserName")
+        public static User GetTestUser(string userName = "TestUserName")
         {
             var testUser = new User
             {
--- test/BackOfficeBase.Tests.Application/Authorization/PermissionAppServiceTests.cs
+++ test/BackOfficeBase.Tests.Application/Authorization/PermissionAppServiceTests.cs
@@ -1,5 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
 using BackOfficeBase.Application.Authorization.Permissions;
+using BackOfficeBase.Domain.AppConsts.Authorization;
+using BackOfficeBase.Domain.Entities.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Moq;
 using Xunit;
 
 namespace BackOfficeBase.Tests.Application.Authorization
@@ -8,16 +15,28 @@ namespace BackOfficeBase.Tests.Application.Authorization
     {
         private readonly IPermissionAppService _permissionAppService;
 
+        private static string TestPermissionClaimForUser = "TestPermissionClaimForUser";
+        private static string TestPermissionClaimForRole = "TestPermissionClaimForRoe";
+
         public PermissionAppServiceTests()
         {
-            _permissionAppService = new PermissionAppService(UserManager, RoleManager);
+            var testUser = GetTestUser();
+            var testRole = GetTestRole();
+            AddUserToRole(testUser, testRole);
+
+            var mockUserClaimStore = SetupMockUserClaimStore(testUser);
+            var mockRoleClaimStore = SetupMockRoleClaimStore(testRole);
+
+            var userManager = new UserManager<User>(mockUserClaimStore.Object, null, null, null, null, null, null, null, null);
+            var roleManager = new RoleManager<Role>(mockRoleClaimStore.Object, null, null, null, null);
+            _permissionAppService = new PermissionAppService(userManager, roleManager);
         }
 
         [Fact]
         public async Task Should_Permission_Granted_To_User()
         {
             var isPermissionGranted =
-                await _permissionAppService.IsUserGrantedToPermissionAsync("TestUserName", "TestPermissionForUserClaim");
+                await _permissionAppService.IsUserGrantedToPermissionAsync("TestUserName", TestPermissionClaimForUser);
 
             Assert.True(isPermissionGranted);
         }
@@ -26,7 +45,7 @@ namespace BackOfficeBase.Tests.Application.Authorization
         public async Task Should_Permission_Granted_To_User_Role()
         {
             var isPermissionGranted =
-                await _permissionAppService.IsUserGrantedToPermissionAsync("TestUserName", "TestPermissionForRoleClaim");
+                await _permissionAppService.IsUserGrantedToPermissionAsync("TestUserName", TestPermissionClaimForRole);
 
             Assert.True(isPermissionGranted);
         }
@@ -35,18 +54,34 @@ namespace BackOfficeBase.Tests.Application.Authorization
         public async Task Should_Not_Permission_Granted_To_User()
         {
             var isPermissionNotGranted =
-                await _permissionAppService.IsUserGrantedToPermissionAsync("TestUserName", "TestPermissionForUserClaim1");
+                await _permissionAppService.IsUserGrantedToPermissionAsync("TestUserName", "NotGrantedPermissionClaim");
 
             Assert.False(isPermissionNotGranted);
         }
 
-        [Fact]
-        public async Task Should_Not_Permission_Granted_To_User_Role()
+        private static Mock<IRoleClaimStore<Role>> SetupMockRoleClaimStore(Role testRole)
         {
-            var isPermissionNotGranted =
-                await _permissionAppService.IsUserGrantedToPermissionAsync("TestUserName", "TestPermissionForRoleClaim1");
+            var mockRoleClaimStore = new Mock<IRoleClaimStore<Role>>();
+            mockRoleClaimStore.Setup(x => x.GetClaimsAsync(testRole, CancellationToken.None)).ReturnsAsync(
+                new List<Claim>
+                {
+                    new Claim(CustomClaimTypes.Permission, TestPermissionClaimForRole)
+                });
 
-            Assert.False(isPermissionNotGranted);
+            return mockRoleClaimStore;
+        }
+
+        private static Mock<IUserClaimStore<User>> SetupMockUserClaimStore(User testUser)
+        {
+            var mockUserClaimStore = new Mock<IUserClaimStore<User>>();
+            mockUserClaimStore.Setup(x => x.FindByNameAsync(testUser.UserName, CancellationToken.None)).ReturnsAsync(testUser);
+            mockUserClaimStore.Setup(x => x.GetClaimsAsync(testUser, CancellationToken.None)).ReturnsAsync(
+                new List<Claim>
+                {
+                    new Claim(CustomClaimTypes.Permission, TestPermissionClaimForUser)
+                });
+
+            return mockUserClaimStore;
         }
     }
 }
