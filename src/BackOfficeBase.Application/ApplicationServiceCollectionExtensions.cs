cc1e903ec1f668b65092f4f9454d9a3c00a0f7fd
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2020-04-22 03:31:56 +0300

    Create ApplicationServiceCollectionExtensions.cs

---
+++ src/BackOfficeBase.Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BackOfficeBase.Application
+{
+    public static class ApplicationServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureNucleusApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(ApplicationServiceCollectionExtensions));
+
+            //services.RegisterAssemblyPublicNonGenericClasses(Assembly.GetExecutingAssembly())
+            //    .Where(c => c.Name.EndsWith("AppService"))
+            //    .AsPublicImplementedInterfaces();
+
+            return services;
+        }
+    }
+}
