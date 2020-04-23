844d383e000a4affe0f019df7ce7b2164301dcdb
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2020-04-24 01:21:45 +0300

    Added serilog

--- src/BackOfficeBase.Application/Email/EmailSender.cs
+++ src/BackOfficeBase.Application/Email/EmailSender.cs
@@ -1,60 +1,47 @@
-﻿using System;
-using System.Threading.Tasks;
-using MailKit.Net.Smtp;
-using Microsoft.Extensions.Logging;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using MimeKit;
+using Serilog;
+#if !DEBUG
+using MailKit.Net.Smtp;
+#endif
 
 namespace BackOfficeBase.Application.Email
 {
     public class EmailSender : IEmailSender
     {
         private readonly EmailSettings _emailSettings;
-        private readonly ILogger _logger;
 
         public EmailSender(
-            IOptions<EmailSettings> emailSettings, ILogger logger)
+            IOptions<EmailSettings> emailSettings)
         {
-            _logger = logger;
             _emailSettings = emailSettings.Value;
         }
 
-        // TODO: Write test
         public async Task SendEmailAsync(string email, string subject, string message)
         {
-            try
+            var mimeMessage = new MimeMessage();
+            mimeMessage.From.Add(new MailboxAddress(_emailSettings.SenderUsername, _emailSettings.SenderEmail));
+            mimeMessage.To.Add(new MailboxAddress(email));
+            mimeMessage.Subject = subject;
+            mimeMessage.Body = new TextPart("html")
             {
-                var mimeMessage = new MimeMessage();
-                mimeMessage.From.Add(new MailboxAddress(_emailSettings.SenderUsername, _emailSettings.SenderEmail));
-                mimeMessage.To.Add(new MailboxAddress(email));
-                mimeMessage.Subject = subject;
-                mimeMessage.Body = new TextPart("html")
-                {
-                    Text = message
-                };
+                Text = message
+            };
 
 #if DEBUG
-                _logger.LogInformation(@$"-------- Send Email: From: {_emailSettings.SenderEmail} - To: {email}
+            Log.Information(@$"-------- Send Email: From: {_emailSettings.SenderEmail} - To: {email}
                                        \n -------- Subject: {subject}
                                        \n -------- Body: {message}");
 #else
-                using (var client = new SmtpClient())
-                {
-                    await client.ConnectAsync(_emailSettings.Host, _emailSettings.Port, _emailSettings.EnableSsl);
-                    await client.AuthenticateAsync(_emailSettings.SenderEmail, _emailSettings.Password);
-                    await client.SendAsync(mimeMessage);
-                    await client.DisconnectAsync(true);
-                }
-#endif
-
-
-
-            }
-            catch (Exception ex)
+            using (var client = new SmtpClient())
             {
-                // TODO: handle exception
-                throw new InvalidOperationException(ex.Message);
+                await client.ConnectAsync(_emailSettings.Host, _emailSettings.Port, _emailSettings.EnableSsl);
+                await client.AuthenticateAsync(_emailSettings.SenderEmail, _emailSettings.Password);
+                await client.SendAsync(mimeMessage);
+                await client.DisconnectAsync(true);
             }
+#endif
         }
     }
 }
--- src/BackOfficeBase.Web.Api/Program.cs
+++ src/BackOfficeBase.Web.Api/Program.cs
@@ -1,26 +1,60 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace BackOfficeBase.Web.Api
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    "Logs/Logs.txt",
+                    fileSizeLimitBytes: 1_000_000,
+                    rollOnFileSizeLimit: true,
+                    shared: true,
+                    flushToDiskInterval: TimeSpan.FromSeconds(1))
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                var host = CreateHostBuilder(args).Build();
+                using (var scope = host.Services.CreateScope())
+                {
+                    // TODO: Seed database
+                    //var dbContext = scope.ServiceProvider.GetRequiredService<BackOfficeBaseDbContext>();
+                    //new DbContextDataSeeder(dbContext).SeedData();
+                }
+
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                 });
     }
 }
