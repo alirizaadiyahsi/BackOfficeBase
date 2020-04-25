355514bb9d365348140c9830bfae32ff4509c088
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2020-04-26 02:17:05 +0300

    Update User.cs

--- src/BackOfficeBase.Domain/Entities/Authorization/User.cs
+++ src/BackOfficeBase.Domain/Entities/Authorization/User.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using BackOfficeBase.Domain.Entities.Auditing;
 using BackOfficeBase.Domain.Entities.OrganizationUnits;
 using Microsoft.AspNetCore.Identity;
