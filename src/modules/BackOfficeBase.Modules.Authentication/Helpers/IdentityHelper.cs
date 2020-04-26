febb0a5efd7e52c31404f7bf524e4d57de0802a9
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2020-04-27 01:01:01 +0300

    Update IdentityHelper.cs

--- src/modules/BackOfficeBase.Modules.Authentication/Helpers/IdentityHelper.cs
+++ src/modules/BackOfficeBase.Modules.Authentication/Helpers/IdentityHelper.cs
@@ -32,18 +32,6 @@ namespace BackOfficeBase.Modules.Authentication.Helpers
             return new ClaimsIdentity(new ClaimsIdentity(new GenericIdentity(userNameOrEmail, "Token"), claims));
         }
 
-        private static List<Claim> CreateRoleClaims(User userToVerify, List<Claim> claims)
-        {
-            // add roles to roleClaim to use build-in User.IsInRole method
-            var roleNames = userToVerify.UserRoles.Select(ur => ur.Role.Name);
-            foreach (var roleName in roleNames)
-            {
-                claims.Add(new Claim(ClaimTypes.Role, roleName));
-            }
-
-            return claims;
-        }
-
         private static List<Claim> CreateUserClaims(User userToVerify)
         {
             var claims = new List<Claim>(new[]
@@ -52,6 +40,18 @@ namespace BackOfficeBase.Modules.Authentication.Helpers
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                 new Claim("Id", userToVerify.Id.ToString())
             });
+
+            return claims;
+        }
+
+        private static List<Claim> CreateRoleClaims(User userToVerify, List<Claim> claims)
+        {
+            // add roles to roleClaim to use build-in User.IsInRole method
+            var roleNames = userToVerify.UserRoles.Select(ur => ur.Role.Name);
+            foreach (var roleName in roleNames)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, roleName));
+            }
 
             return claims;
         }
