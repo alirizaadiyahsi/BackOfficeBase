4632367f54e3db2fc8b5c6422adcb82c493d9b08
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2020-04-27 01:48:32 +0300

    Added authorization module project structure

---
+++ src/modules/BackOfficeBase.Modules.Authorization/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using BackOfficeBase.Web.Core.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackOfficeBase.Modules.Authorization.Controllers
+{
+    public class UsersController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<object> GetUsers()
+        {
+            return Ok();
+        }
+    }
+}
