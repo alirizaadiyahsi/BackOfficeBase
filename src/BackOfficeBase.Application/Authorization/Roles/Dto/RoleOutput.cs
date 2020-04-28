26a1f816854d2f6ffaf9856e313278f92980c2e8
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2020-04-28 17:14:55 +0300

    Added roleoutput

---
+++ src/BackOfficeBase.Application/Authorization/Roles/Dto/RoleOutput.cs
@@ -0,0 +1,9 @@
+﻿using BackOfficeBase.Application.Shared.Dto;
+
+namespace BackOfficeBase.Application.Authorization.Roles.Dto
+{
+    public class RoleOutput:EntityDto
+    {
+        public string Name { get; set; }
+    }
+}
--- src/BackOfficeBase.Application/Authorization/Users/Dto/UserOutput.cs
+++ src/BackOfficeBase.Application/Authorization/Users/Dto/UserOutput.cs
@@ -1,4 +1,7 @@
-﻿using BackOfficeBase.Application.Shared.Dto;
+﻿using System;
+using System.Collections.Generic;
+using BackOfficeBase.Application.Authorization.Roles.Dto;
+using BackOfficeBase.Application.Shared.Dto;
 
 namespace BackOfficeBase.Application.Authorization.Users.Dto
 {
@@ -15,5 +18,12 @@ namespace BackOfficeBase.Application.Authorization.Users.Dto
         public string Phone { get; set; }
 
         public string ProfileImageUrl { get; set; }
+
+        // TODO: Map following props in automapper profile
+        public Guid[] SelectedRoleIds { get; set; }
+
+        public Guid[] SelectedClaimIds { get; set; }
+
+        public List<RoleOutput> AllRoles { get; set; }
     }
 }
