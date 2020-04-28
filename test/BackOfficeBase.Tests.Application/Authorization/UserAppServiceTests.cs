aca53896e06658faa24fbef5658da9156aa28502
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2020-04-28 23:19:20 +0300

    Update UserAppServiceTests.cs

--- test/BackOfficeBase.Tests.Application/Authorization/UserAppServiceTests.cs
+++ test/BackOfficeBase.Tests.Application/Authorization/UserAppServiceTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using BackOfficeBase.Application.Authorization.Users;
+using BackOfficeBase.Application.Authorization.Users.Dto;
 using BackOfficeBase.DataAccess;
 using BackOfficeBase.Domain.AppConsts.Authorization;
 using BackOfficeBase.Domain.Entities.Authorization;
@@ -18,14 +19,10 @@ namespace BackOfficeBase.Tests.Application.Authorization
     {
         private readonly IUserAppService _userAppService;
         private readonly TestBackOfficeBaseDbContext _dbContext;
-        private readonly User _testUser = GetTestUser("test_user_for_user_app_service", "test_user_for_user_app_service@mail.com");
-        private readonly Role _testRole = GetTestRole("test_role_for_user_app_service");
 
         public UserAppServiceTests()
         {
-            AddUserToRole(_testUser, _testRole);
             _dbContext = GetTestDbContext();
-
             var mapper = GetNewHostServiceProvider().GetRequiredService<IMapper>();
             _userAppService = new UserAppService(_dbContext, mapper);
         }
@@ -33,23 +30,50 @@ namespace BackOfficeBase.Tests.Application.Authorization
         [Fact]
         public async Task Should_Get_Async()
         {
-            await _dbContext.Users.AddAsync(_testUser);
-            await _dbContext.Roles.AddAsync(_testRole);
+            var testUser = GetTestUser("test_user_for_user_app_service_get", "test_user_for_user_app_service_get@mail.com");
+            var testRole = GetTestRole("test_role_for_user_app_service_get");
+            AddUserToRole(testUser, testRole);
+
+            await _dbContext.Users.AddAsync(testUser);
+            await _dbContext.Roles.AddAsync(testRole);
             await _dbContext.UserClaims.AddAsync(new UserClaim
             {
-                UserId = _testUser.Id,
+                UserId = testUser.Id,
                 ClaimType = CustomClaimTypes.Permission,
                 ClaimValue = AppPermissions.Users.Read
             });
-
             await _dbContext.SaveChangesAsync();
 
-            var userOutput = await _userAppService.GetAsync(_testUser.Id);
+            var userOutput = await _userAppService.GetAsync(testRole.Id);
 
             Assert.NotNull(userOutput);
             Assert.True(userOutput.AllRoles != null && userOutput.AllRoles.Any());
             Assert.True(userOutput.SelectedPermissions != null && userOutput.SelectedPermissions.Length > 0);
             Assert.True(userOutput.SelectedRoleIds != null && userOutput.SelectedRoleIds.Length > 0);
+        }
+
+        [Fact]
+        public async Task Should_Create_Async()
+        {
+            var testRole = GetTestRole("test_role_for_user_app_service_create");
+            await _dbContext.Roles.AddAsync(testRole);
+            await _dbContext.SaveChangesAsync();
+
+            var createUserInput = new CreateUserInput
+            {
+                UserName = "test_user_for_user_app_service_create",
+                Email = "test_user_for_user_app_service_create@mail.com",
+                SelectedRoleIds = new[] { testRole.Id },
+                SelectedPermissions = new[] { AppPermissions.Users.Read }
+            };
+            var userOutput = await _userAppService.CreateAsync(createUserInput);
+            await _dbContext.SaveChangesAsync();
+
+            var insertedUser = await GetTestDbContext().Users.FindAsync(userOutput.Data.Id);
+
+            Assert.NotNull(insertedUser);
+            Assert.True(insertedUser.UserRoles != null && insertedUser.UserRoles.Any());
+            Assert.True(insertedUser.UserClaims != null && insertedUser.UserClaims.Any());
         }
     }
 }
