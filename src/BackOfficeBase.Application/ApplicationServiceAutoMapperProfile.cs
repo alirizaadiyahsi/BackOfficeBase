37014c9d23100258afe77a75ea40fce8071d3b9f
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2020-04-30 00:47:52 +0300

    Update ApplicationServiceAutoMapperProfile.cs

--- src/BackOfficeBase.Application/ApplicationServiceAutoMapperProfile.cs
+++ src/BackOfficeBase.Application/ApplicationServiceAutoMapperProfile.cs
@@ -14,26 +14,17 @@ namespace BackOfficeBase.Application
         {
             CreateMap<User, UserOutput>()
                 .ForMember(dest => dest.SelectedRoleIds,
-                    opt => opt.MapFrom((entity, dto, _, context) =>
-                    {
-                        return entity.UserRoles.Where(ur => ur.UserId == Guid.Parse(context.Items["UserId"].ToString())).Select(ur => ur.RoleId);
-                    }))
-                .ForMember(dest => dest.SelectedPermissions,
-                    opt => opt.MapFrom((entity, dto, _, context) =>
-                    {
-                        return entity.UserClaims.Where(uc => uc.UserId == Guid.Parse(context.Items["UserId"].ToString()) && uc.ClaimType == CustomClaimTypes.Permission).Select(uc => uc.ClaimValue);
-                    }));
-
-            CreateMap<User, UserOutput>()
-                .ForMember(dest => dest.SelectedRoleIds,
                     opt => opt.MapFrom(src => src.UserRoles.Select(x => x.RoleId)))
                 .ForMember(dest => dest.SelectedPermissions,
                     opt => opt.MapFrom(src => src.UserClaims.Where(uc => uc.ClaimType == CustomClaimTypes.Permission).Select(uc => uc.ClaimValue)));
-
             CreateMap<CreateUserInput, User>();
             CreateMap<UpdateUserInput, User>();
 
-            CreateMap<Role, RoleOutput>();
+            CreateMap<Role, RoleOutput>()
+                .ForMember(dest => dest.SelectedPermissions,
+                    opt => opt.MapFrom(src => src.RoleClaims.Where(uc => uc.ClaimType == CustomClaimTypes.Permission).Select(uc => uc.ClaimValue)));
+            CreateMap<CreateRoleInput, Role>();
+            CreateMap<UpdateRoleInput, Role>();
         }
     }
 }
