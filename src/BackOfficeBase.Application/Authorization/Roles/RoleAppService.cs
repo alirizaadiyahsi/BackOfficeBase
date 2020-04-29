bd29e553bfe2b68ab70163801390e3e3e353e3c8
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2020-04-30 00:47:57 +0300

    Update RoleAppService.cs

--- src/BackOfficeBase.Application/Authorization/Roles/RoleAppService.cs
+++ src/BackOfficeBase.Application/Authorization/Roles/RoleAppService.cs
@@ -1,13 +1,18 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
 using BackOfficeBase.Application.Authorization.Roles.Dto;
 using BackOfficeBase.Application.Shared.Services;
 using BackOfficeBase.DataAccess;
+using BackOfficeBase.Domain.AppConstants.Authorization;
 using BackOfficeBase.Domain.Entities.Authorization;
 
 namespace BackOfficeBase.Application.Authorization.Roles
 {
     // TODO: Write test
-    public class RoleAppService :CrudAppService<Role, RoleOutput, RoleListOutput, CreateRoleInput, UpdateRoleInput>, IRoleAppService
+    public class RoleAppService : CrudAppService<Role, RoleOutput, RoleListOutput, CreateRoleInput, UpdateRoleInput>, IRoleAppService
     {
         private readonly BackOfficeBaseDbContext _dbContext;
         private readonly IMapper _mapper;
@@ -16,6 +21,55 @@ namespace BackOfficeBase.Application.Authorization.Roles
         {
             _dbContext = dbContext;
             _mapper = mapper;
+        }
+
+        public override async Task<RoleOutput> GetAsync(Guid id)
+        {
+            var roleOutput = await base.GetAsync(id);
+            roleOutput.AllPermissions = AppPermissions.GetAll();
+
+            return roleOutput;
+        }
+
+        public override async Task<RoleOutput> CreateAsync(CreateRoleInput input)
+        {
+            var roleOutput = await base.CreateAsync(input);
+
+            AddPermissionsToRole(input.SelectedPermissions, roleOutput.Id);
+            SetSelectedNavigationProperties(input.SelectedPermissions, roleOutput);
+
+            return roleOutput;
+        }
+
+        public override RoleOutput Update(UpdateRoleInput input)
+        {
+            var roleOutput = base.Update(input);
+
+            _dbContext.RoleClaims.RemoveRange(_dbContext.RoleClaims.Where(x => x.RoleId == input.Id && x.ClaimType == CustomClaimTypes.Permission));
+            _dbContext.SaveChanges();
+
+            AddPermissionsToRole(input.SelectedPermissions, roleOutput.Id);
+            SetSelectedNavigationProperties(input.SelectedPermissions, roleOutput);
+
+            return roleOutput;
+        }
+
+        private static void SetSelectedNavigationProperties(IEnumerable<string> selectedPermissions, RoleOutput roleOutput)
+        {
+            roleOutput.SelectedPermissions = selectedPermissions;
+        }
+
+        private void AddPermissionsToRole(IEnumerable<string> selectedPermissions, Guid roleId)
+        {
+            foreach (var permission in selectedPermissions)
+            {
+                _dbContext.RoleClaims.Add(new RoleClaim
+                {
+                    RoleId = roleId,
+                    ClaimType = CustomClaimTypes.Permission,
+                    ClaimValue = permission
+                });
+            }
         }
     }
 }
